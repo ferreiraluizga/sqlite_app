dc47bb24c2e2c172f3944aa099352c55952f9cd7
ferreiraluizga <luiz.gabriel.lcf@gmail.com>
2025-05-16 12:38:11 -0300

    feat: getting data of layout in main activity_main.xml

--- app/src/main/java/com/example/sqlite_app/MainActivity.kt
+++ app/src/main/java/com/example/sqlite_app/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.sqlite_app
 
+import android.os.Build
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.time.LocalDate
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val edtNome:EditText = findViewById(R.id.edtNome)
+        val edtEndereco:EditText = findViewById(R.id.edtEndereco)
+        val edtBairro:EditText = findViewById(R.id.edtBairro)
+        val edtCep:EditText = findViewById(R.id.edtCep)
+        val edtObs:EditText = findViewById(R.id.edtObs)
+        val dataCadastro:String = LocalDate.now().toString()
+
+        val btnCadastrar:Button = findViewById(R.id.btnCadastrar)
+
     }
 }
